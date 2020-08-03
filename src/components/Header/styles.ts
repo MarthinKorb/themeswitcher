7bb8aae4d773d093bf768e32da9fbc14c8115560
Marthin Korb <marthinkorb@gmail.com>
2020-08-03 09:19:04 -0300

    Adicionando componentes nos styles

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 export const Container = styled.div`
   height: 60px;
@@ -10,3 +10,40 @@ export const Container = styled.div`
   justify-content: space-between;
   padding: 0 30px;
 `;
+
+export const Container2 = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
+const animate = keyframes`
+  from {
+      opacity: 0;
+      transform: translateY(1000px);
+    }
+  to{
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+export const ImageContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;  
+  margin-top: 150px;
+  height: 300px;
+  width: 300px;
+  background: ${props => props.theme.colors.secundary};
+  border: 10px solid ${props => props.theme.colors.third};
+  border-radius: 30%;
+
+  animation: ${animate} 750ms;
+`;
+
+
+
+
