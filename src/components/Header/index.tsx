ff47568323163784262af0f32afa9e959eb5723c
Marthin Korb <marthinkorb@gmail.com>
2020-08-03 09:18:30 -0300

    Configurando e estilizando o componente Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react';
-import { Container } from './styles';
+import { Container, Container2, ImageContainer } from './styles';
+
+import { DoneOutline } from '@material-ui/icons';
 
 import { shade } from 'polished';
 
@@ -15,22 +17,32 @@ const Header: React.FC<Props> = ({ toggleTheme }) => {
 
     const { title, colors } = useContext(ThemeContext);
 
-    
+
     return (
-        <Container>
-            <h2>Switch Theme With Styled Components</h2>
-            <Switch
-                onChange={toggleTheme}
-                checked={title === 'dark'}
-                checkedIcon={false}
-                uncheckedIcon={false}
-                height={10}
-                width={40}
-                handleDiameter={20}
-                onColor={shade(0.3, colors.primary)}
-                offColor={colors.primary}
-            />
-        </Container>
+        <>
+            <Container>
+                <h2>Switch Theme With Styled Components</h2>
+                <Switch
+                    onChange={toggleTheme}
+                    checked={title === 'dark'}
+                    checkedIcon={false}
+                    uncheckedIcon={false}
+                    height={10}
+                    width={40}
+                    handleDiameter={20}
+                    onHandleColor={colors.third}
+                    offHandleColor={colors.third}
+                    onColor={shade(0.3, colors.primary)}
+                    offColor={colors.primary}
+                />
+            </Container>
+
+            <Container2>
+            <ImageContainer>
+                <DoneOutline fontSize="large"/>
+            </ImageContainer>
+            </Container2>
+        </>
     );
 }
 
